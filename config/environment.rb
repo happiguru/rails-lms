73c88fa9b9c75c2916ff1907c34c079054b3d832
happiguru <hguruman@gmail.com>
2021-12-03 21:59:26 +0000

    COnfigure mailer using sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['whomgodsent@gmail.com'],
+    :password => ENV['but1eneENOW@lekunzestanley'],
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "c76751e7a07a4dc0bc6c04d65ce6badd.vfs.cloud9.us-east-2.amazonaws.com"
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://c76751e7a07a4dc0bc6c04d65ce6badd.vfs.cloud9.us-east-2.amazonaws.com' }
+  config.action_mailer.delivery_method = :test
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
