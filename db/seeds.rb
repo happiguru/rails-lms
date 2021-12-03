ce27f0708cb7c5d8698d17d341b6f83ee2fae9c5
happiguru <hguruman@gmail.com>
2021-12-03 22:32:13 +0000

    Modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
+# User.create!(email: 'hguruman@gmail.com', password: 'admin@example.com', password_confirmation: 'admin@example.com')
 
 30.times do
     Course.create!([{
